d36116417dcc27a435f73633a4b54185b744771a
wei.li@feedhenry.com <wei.li@feedhenry.com>
2012-03-22 13:18:37 +0000

    update file /client/default/init.js

--- client/default/init.js
+++ client/default/init.js
@@ -3,3 +3,21 @@ function testWebCall(){
     alert(JSON.stringify(res));
   })
 }
+
+function testCreateCache(){
+  $fh.act({act:'testCreateCache', req:{}}, function(res){
+    alert(JSON.stringify(res));
+  })
+}
+
+function testLoadCache(){
+  $fh.act({act:'testLoadCache', req:{}}, function(res){
+    alert(JSON.stringify(res));
+  })
+}
+
+function testRemoveCache(){
+  $fh.act({act:'testRemoveCache', req:{}}, function(res){
+    alert(JSON.stringify(res));
+  })
+}
